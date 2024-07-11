f1eae6465fdb5392e726698b188e1044b83b9991
Alif Panglima Nurda <alifnurda@gmail.com>
2024-07-11 20:58:20 +0700

    Update Insert File Input in Career Form Page

--- public/js/styles.js
+++ public/js/styles.js
@@ -7,3 +7,11 @@ listContent.forEach((value) => {
     value.classList.toggle('open'); // Toggles between .list.open ... in style
   });
 });
+
+// Get CV or Resume File Name
+const getName = document.getElementById('getFileName')
+const setName = document.getElementById('setFileName')
+
+setName.addEventListener("input", function (event) {
+  getName.textContent = event.target.value.replace(/^C:\\fakepath\\/, "")
+})
